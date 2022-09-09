4b02dfdacc1ec4ef0c6928b9f53da81f65b35b5b
Shlomo Alexander Perlov <perlov3301@gmail.com>
2022-09-09 16:17:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/basic220909/readme.kt
@@ -0,0 +1,7 @@
+package com.example.basic220909
+
+class readme {
+    // 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
+    // https://{TOKEN}@github.com/{USER}/{REPO}.git
+    // https://{token}@github.com/{perlov3301}/basic220909.git
+}
