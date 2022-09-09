02acc2e92f8d8c2f477a1273dc3435a8f2a04302
Shlomo Alexander Perlov <perlov3301@gmail.com>
2022-09-09 16:23:20 +0300

    tools remotes token@github.com

--- app/src/main/java/com/example/basic220909/readme.kt
+++ app/src/main/java/com/example/basic220909/readme.kt
@@ -3,6 +3,8 @@ package com.example.basic220909
 class readme {
     // settings github -> "+ with token" -> create token
     // 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
+    // tools remotes url:
     // https://{TOKEN}@github.com/{USER}/{REPO}.git
     // https://{token}@github.com/{perlov3301}/basic220909.git
+// origin	https://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@github.com/perlov3301/thirdemptygithub.git
 }
